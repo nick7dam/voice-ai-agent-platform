3833d90c2715146c59c1d980d0a6cd955fd75d7d
nick7dam <nick7dam@gmail.com>
2026-04-17 23:31:56 +1000

    adding changes

--- src/modules/tts/tts-adapter.interface.ts
+++ src/modules/tts/tts-adapter.interface.ts
@@ -1,8 +1,16 @@
 import {
   TtsAudioResult,
+  TtsAudioStreamChunk,
+  TtsAudioStreamResult,
   TtsSynthesisInput,
 } from '../../common/types/tts.types';
 
 export interface TtsAdapter {
   synthesize(input: TtsSynthesisInput): Promise<TtsAudioResult>;
+  stream?(
+    input: TtsSynthesisInput,
+    callbacks: {
+      onChunk: (chunk: TtsAudioStreamChunk) => void | Promise<void>;
+    },
+  ): Promise<TtsAudioStreamResult>;
 }
